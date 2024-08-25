7eb97b6589c50afd96950e8925f49b7ad5238a13
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2024-08-25 20:45:45 +0300

    Init CsRankings Scraper

---
+++ scrape_website/csrankings_scraper.py
@@ -0,0 +1,3 @@
+"""Get the professor links from CSRankings Website"""
+
+URL = "https://csrankings.org/#/index?ai&vision&mlmining&nlp&inforet&robotics&bio&ecom&world"  #Based on your needs you can use another url from csrankings
