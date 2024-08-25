81f69f280ee24cffd90bec17ca5fa4b13136c269
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2024-08-26 00:12:59 +0300

    Add Async Scraper for Csrankings

--- scrape_website/csrankings_scraper.py
+++ scrape_website/csrankings_scraper.py
@@ -1,3 +1,41 @@
 """Get the professor links from CSRankings Website"""
 
-URL = "https://csrankings.org/#/index?ai&vision&mlmining&nlp&inforet&robotics&bio&ecom&world"  #Based on your needs you can use another url from csrankings
+
+import asyncio
+import pprint
+
+from bs4 import BeautifulSoup
+from playwright.async_api import async_playwright
+
+async def ascrape_playwright(url):
+    """
+    An asynchronous Python function that uses Playwright to scrape
+    content from a given URL, extracting specified HTML tags and removing unwanted tags and unnecessary
+    lines.
+    """
+    print("Started scraping...")
+    results = ""
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        try:
+            page = await browser.new_page()
+            await page.goto(url)
+
+            page_source = await page.content()
+
+
+            print("URLs scraped")
+        except Exception as e:
+            results = f"Error: {e}"
+        await browser.close()
+    return results
+
+
+if __name__ == "__main__":
+    url = "https://csrankings.org/#/index?ai&vision&mlmining&nlp&inforet&robotics&bio&ecom&world"
+
+    async def scrape_playwright():
+        results = await ascrape_playwright(url)
+        print(results)
+
+    pprint.pprint(asyncio.run(scrape_playwright()))
