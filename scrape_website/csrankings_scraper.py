fe3ae916958bdec8b0d4d378f0ab4f9c4c3ea134
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2024-08-26 15:52:50 +0300

    Cs Ranking Scraper Updated, Professor Class Created

--- scrape_website/csrankings_scraper.py
+++ scrape_website/csrankings_scraper.py
@@ -7,6 +7,8 @@ import pprint
 from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright
 
+from professor import Professor
+
 async def ascrape_playwright(url):
     """
     An asynchronous Python function that uses Playwright to scrape
@@ -17,18 +19,36 @@ async def ascrape_playwright(url):
     results = ""
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True)
+
+        url_dict = {}  #dict of dictionaries
+
         try:
             page = await browser.new_page()
             await page.goto(url)
 
             page_source = await page.content()
 
+            # Use BeautifulSoup to parse HTML
+            soup = BeautifulSoup(page_source, 'html.parser')
+            
+            # Convert the XPath to a CSS selector
+            element = soup.select_one('body > div:nth-of-type(5) > form > div > div:nth-of-type(2) > div:nth-of-type(2) > div > div > table > tbody')
+            
+            print(type(element))
 
-            print("URLs scraped")
+        
         except Exception as e:
             results = f"Error: {e}"
         await browser.close()
-    return results
+    
+    return results #change this
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
---
+++ scrape_website/professor.py
@@ -0,0 +1,10 @@
+#struct with prof properties
+
+class Professor:
+
+    def __init__(self, name = "", area = "", url = ""):
+
+        self.name = name
+        self.area = area
+        self.url = url
+
