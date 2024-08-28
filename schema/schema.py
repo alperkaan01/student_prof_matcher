a6af926278e178184513a583042c207eaa626ad6
alperkaan01 <88709260+alperkaan01@users.noreply.github.com>
2024-08-28 22:44:38 +0300

    Implemented website reader and environment file

--- schema/schema.py
+++ schema/schema.py
@@ -1,1 +1,29 @@
 #create a schema for langchain extraction chain
+from typing import Optional
+
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+class ProfProfile(BaseModel):
+    """Information about a person."""
+
+    # ^ Doc-string for the entity Person.
+    # This doc-string is sent to the LLM as the description of the schema Person,
+    # and it can help to improve extraction results.
+
+    # Note that:
+    # 1. Each field is an `optional` -- this allows the model to decline to extract it!
+    # 2. Each field has a `description` -- this description is used by the LLM.
+    # Having a good description can help improve extraction results.
+    about_me: Optional[str] = Field(default=None, description="Description of the Professor which summarizes his or her interests. It could be located under the about me section or it could directly emerge as a paragraph on the top of the page.")
+
+    research_interests: Optional[str] = Field(
+        default=None, description="The topic list of the research interests of the professor if known."
+    )
+    
+    research_list: Optional[str] = Field(
+        default=None, description="Titles of the research professor have conducted. Brief description of each research if known."
+    )
+
+    previous_students: Optional[str] = Field(
+        default=None, description= "Previous student alumnis and their positions if known"
+    )
--- scrape_website/scraper.py
+++ scrape_website/scraper.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 
 import json
 
+from langchain_community.document_loaders import AsyncChromiumLoader
+from langchain_community.document_transformers import BeautifulSoupTransformer
+
 
 def scrape_prof_websites():
 
@@ -13,6 +16,7 @@ def scrape_prof_websites():
 
         # Retrieve environment variables
         data_path = os.getenv('DATA_PATH')
+        user_agent = os.getenv('USER_AGENT')
 
         with open(data_path, 'r', encoding='utf-8') as f:
             print("Loading Data ...")
@@ -22,7 +26,34 @@ def scrape_prof_websites():
                 # Print the loaded data or process it as needed
         #print(data)
         
-        
+
+        for k,v in data.items():
+            university_name = k
+
+            for prof in v:
+                name = prof['name']
+                research_area = prof['area']
+                url = prof['url']
+
+                print(url)
+            
+                loader = AsyncChromiumLoader([url], user_agent=user_agent)
+                html = loader.load()
+
+                bs_transformer = BeautifulSoupTransformer()
+                docs_transformed = bs_transformer.transform_documents(html, tags_to_extract=["li", "a", "span", "div", "h1", "h2", "h3"])
+
+                extracted_data = docs_transformed[0].page_content[0:16000]
+
+                print(extracted_data)
+
+                break
+
+            break
+
+
+
+
 
 
     except Exception as e:
